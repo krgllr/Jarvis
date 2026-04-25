2cb70a79ab5b26e7a407ce69d29c2234eff15b1b
krgllr <mervegonuller@yahoo.com>
2026-04-25 21:23:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE = "jarvis-v2";
+const CACHE = "jarvis-v3";
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(cache =>
       cache.addAll([
@@ -12,8 +13,17 @@ self.addEventListener("install", e => {
   );
 });
 
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
